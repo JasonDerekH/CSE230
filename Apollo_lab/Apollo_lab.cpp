ee547a49901ea51e3d23b6cc8193dd7ad89ad605
JasonDerekH <hol23053@byui.edu>
2026-09-24 15:57:49 -0600

    Put in all the basic functions. Starting to insert calls for said functions.

--- Apollo_lab/Apollo_lab.cpp
+++ Apollo_lab/Apollo_lab.cpp
@@ -1,32 +1,214 @@
 // Apollo_lab.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+/*************************************************************
+ * 1. Name:
+ *      Jason Hollingsworth & Riley Cluff
+ * 2. Assignment Name:
+ *      Practice 02: Physics simulator
+ * 3. Assignment Description:
+ *      Compute how the Apollo lander will move across the screen
+ * 4. What was the hardest part? Be as specific as possible.
+ *      -a paragraph or two about how the assignment went for you-
+ * 5. How long did it take for you to complete the assignment?
+ *      -total time in hours: reading the assignment, submitting, etc.
+ **************************************************************/
+
+#include <iostream>  // for CIN and COUT
+#include <cmath>     // for pow() and sqrt()
+#include <numbers>   // for pi
 using namespace std;
 
-/******************************
-* Prompts user for our numbers *
-*******************************/
-cout << "What is your vertical velocity(m / s) ? " << end1;
-//insert cin statement
+#define WEIGHT   15103.000   // Weight in KG
+#define GRAVITY     -1.625   // Vertical acceleration due to gravity, in m/s^2
+#define THRUST   45000.000   // Thrust of main engine, in Newtons (kg m/s^2)
 
-cout << "What is your horizontal velocity(m / s) ? " << end1;
-//insert cin statement
+/***************************************************
+ * COMPUTE DISTANCE
+ * Apply inertia to compute a new position using the distance equation.
+ * The equation is:
+ *     s = s + v t + 1/2 a t^2
+ * INPUT
+ *     s : original position, in meters
+ *     v : velocity, in meters/second
+ *     a : acceleration, in meters/second^2
+ *     t : time, in seconds
+ * OUTPUT
+ *     s : new position, in meters
+ **************************************************/
+double compute_distance(double s, double v, double a, int t)
+{
+    return s + v * t + .5 * a * pow(t,2);
+ }
 
-cout << "What is your altitude(m) ? " << end1;
-//insert cin statement
+ /**************************************************
+  * COMPUTE ACCELERATION
+  * Find the acceleration given a thrust and mass.
+  * This will be done using Newton's second law of motion:
+  *     f = m * a
+  * INPUT
+  *     f : force, in Newtons (kg * m / s^2)
+  *     m : mass, in kilograms
+  * OUTPUT
+  *     a : acceleration, in meters/second^2
+  ***************************************************/
+double compute_acceleration(double f, double m)
+{
+    return f * m;
+ }
 
-cout << "What is the angle of the LM where 0 is up(degrees) ? " << end1;
-//insert cin statement
+  /***********************************************
+   * COMPUTE VELOCITY
+   * Starting with a given velocity, find the new
+   * velocity once acceleration is applied. This is
+   * called the Kinematics equation. The
+   * equation is:
+   *     v = v + a t
+   * INPUT
+   *     v : velocity, in meters/second
+   *     a : acceleration, in meters/second^2
+   *     t : time, in seconds
+   * OUTPUT
+   *     v : new velocity, in meters/second
+   ***********************************************/
+double compute_velocity(double v, double a, int t)
+{
+    return a * t + v;
+}
 
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+   /***********************************************
+    * COMPUTE VERTICAL COMPONENT
+    * Find the vertical component of a velocity or acceleration.
+    * The equation is:
+    *     cos(a) = y / total
+    * This can be expressed graphically:
+    *      x
+    *    +-----
+    *    |   /
+    *  y |  / total
+    *    |a/
+    *    |/
+    * INPUT
+    *     a : angle, in radians
+    *     total : total velocity or acceleration
+    * OUTPUT
+    *     y : the vertical component of the total
+    ***********************************************/
+double compute_ycomponent(double angle, double total_speed)
+{
+    //returns y
+    return cos(angle) * total_speed;
+   }
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    /***********************************************
+     * COMPUTE HORIZONTAL COMPONENT
+     * Find the horizontal component of a velocity or acceleration.
+     * The equation is:
+     *     sin(a) = x / total
+     * This can be expressed graphically:
+     *      x
+     *    +-----
+     *    |   /
+     *  y |  / total
+     *    |a/
+     *    |/
+     * INPUT
+     *     a : angle, in radians
+     *     total : total velocity or acceleration
+     * OUTPUT
+     *     x : the vertical component of the total
+     ***********************************************/
+double compute_xcomponent(double angle, double total_speed)
+{
+    return sin(angle) * total_speed;
+   }
+
+     /************************************************
+      * COMPUTE TOTAL COMPONENT
+      * Given the horizontal and vertical components of
+      * something (velocity or acceleration), determine
+      * the total component. To do this, use the Pythagorean Theorem:
+      *    x^2 + y^2 = t^2
+      * where:
+      *      x
+      *    +-----
+      *    |   /
+      *  y |  / total
+      *    | /
+      *    |/
+      * INPUT
+      *    x : horizontal component
+      *    y : vertical component
+      * OUTPUT
+      *    total : total component
+      ***********************************************/
+double total_component(double x, double y)
+{
+    return sqrt(pow(x,2) + pow(y,2));
+      }
+
+
+      /*************************************************
+       * RADIANS FROM DEGEES
+       * Convert degrees to radians:
+       *     radians / 2pi = degrees / 360
+       * INPUT
+       *     d : degrees from 0 to 360
+       * OUTPUT
+       *     r : radians from 0 to 2pi
+       **************************************************/
+double get_radians(double d)
+{
+    return d / 360 * 2 * numbers::pi;
+       }
+
+       /**************************************************
+        * PROMPT
+        * A generic function to prompt the user for a double
+        * INPUT
+        *      message : the message to display to the user
+        * OUTPUT
+        *      response : the user's response
+        ***************************************************/
+double prompt(char* question)
+{
+    cout << question;
+    cin >> response;
+}
+
+        /****************************************************************
+         * MAIN
+         * Prompt for input, compute new position, and display output
+         ****************************************************************/
+int main()
+{
+    // Prompt for input and variables to be computed
+    double dx = prompt("What is your horizontal velocity (m/s)? ");
+    double dy = prompt("What is your vertical velocity (m/s)? ");
+    double y = prompt("What is your altitude (m)? ");
+    double x = prompt("What is your position (m)? ");
+    double aDegrees = prompt("What is the angle of the LM where 0 is up (degrees)? ");
+    double t = prompt("What is the time interval (s)? ");
+    double aRadians;            // Angle in radians
+    double accelerationThrust;  // Acceleration due to thrust 
+    double ddxThrust;           // Horizontal acceleration due to thrust
+    double ddyThrust;           // Vertical acceleration due to thrust
+    double ddx;                 // Total horizontal acceleration
+    double ddy;                 // Total vertical acceleration
+    double v;                   // Total velocity
+
+    // Go through the simulator five times
+      // your code goes here
+      // Hint: Update the position _before_ updating the velocity
+
+      // Output
+    cout.setf(ios::fixed | ios::showpoint);
+    cout.precision(2);
+    cout << "\tNew position:   (" << x << ", " << y << ")m\n";
+    cout << "\tNew velocity:   (" << dx << ", " << dy << ")m/s\n";
+    cout << "\tTotal velocity:  " << v << "m/s\n\n";
+
+
+    return 0;
+}
